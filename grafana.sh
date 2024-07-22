1a6b4dcb68ec0b28002f15a17fa30a758abd7e83
Tekade Sukant <59193514+tekadesukant@users.noreply.github.com>
2024-07-23 00:36:04 +0530

    Update grafana.sh

--- grafana.sh
+++ grafana.sh
@@ -2,7 +2,7 @@
 sudo apt-get install -y adduser libfontconfig1
 
 # Fetched latest Grafana Version
-LATEST_VERSION=11.1.0 
+LATEST_VERSION=
 
 # Construct the download URL for the latest version
 DOWNLOAD_URL="https://dl.grafana.com/enterprise/release/grafana-enterprise_${LATEST_VERSION}_amd64.deb"
