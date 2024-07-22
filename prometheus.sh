ed3840e648f0a41a5aea429863f5d7cbbf119f13
Tekade Sukant <59193514+tekadesukant@users.noreply.github.com>
2024-07-23 00:06:47 +0530

    Update prometheus.sh

--- prometheus.sh
+++ prometheus.sh
@@ -1,1 +1,75 @@
+#!/bin/bash
 
+# Fetched Latest Prometheus Version
+LATEST_VERSION=2.53.1 
+
+# Construct the download URL
+DOWNLOAD_URL="https://github.com/prometheus/prometheus/releases/download/v${PROM_V_LATEST_VERSION}/prometheus-${LATEST_VERSION}.linux-amd64.tar.gz"
+
+# Download the latest version of Prometheus
+wget $DOWNLOAD_URL
+
+# Extract the downloaded tarball
+tar -xf prometheus-${LATEST_VERSION}.linux-amd64.tar.gz
+
+# Move the binaries to /usr/local/bin
+sudo mv prometheus-${LATEST_VERSION}.linux-amd64/prometheus prometheus-${LATEST_VERSION}.linux-amd64/promtool /usr/local/bin
+
+# Create directories for configuration files and other Prometheus data
+sudo mkdir -p /etc/prometheus /var/lib/prometheus
+
+# Move console libraries
+sudo mv prometheus-${LATEST_VERSION}.linux-amd64/console_libraries /etc/prometheus
+
+# Clean up the extracted files
+sudo rm -rvf prometheus-${LATEST_VERSION}.linux-amd64*
+
+# Create Prometheus configuration file
+sudo cat <<EOF | sudo tee /etc/prometheus/prometheus.yml
+global:
+  scrape_interval: 10s
+
+scrape_configs:
+  - job_name: 'prometheus_metrics'
+    scrape_interval: 5s
+    static_configs:
+      - targets: ['localhost:9090']
+  - job_name: 'node_exporter_metrics'
+    scrape_interval: 5s
+    static_configs:
+      - targets: ['localhost:9100','worker-1:9100','worker-2:9100']
+EOF
+
+# Create Prometheus user
+sudo useradd -rs /bin/false prometheus
+
+# Change ownership of the directories
+sudo chown -R prometheus: /etc/prometheus /var/lib/prometheus
+
+# Create systemd service file for Prometheus
+sudo cat <<EOF | sudo tee /etc/systemd/system/prometheus.service
+[Unit]
+Description=Prometheus
+After=network.target
+
+[Service]
+User=prometheus
+Group=prometheus
+Type=simple
+ExecStart=/usr/local/bin/prometheus \
+    --config.file /etc/prometheus/prometheus.yml \
+    --storage.tsdb.path /var/lib/prometheus/ \
+    --web.console.templates=/etc/prometheus/consoles \
+    --web.console.libraries=/etc/prometheus/console_libraries
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Reload systemd and enable Prometheus service
+sudo systemctl daemon-reload
+sudo systemctl enable prometheus
+
+# Start Prometheus service and check status
+sudo systemctl start prometheus
+sudo systemctl status prometheus --no-pager
