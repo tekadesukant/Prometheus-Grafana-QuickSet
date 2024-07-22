e4fb79a82803f5e2fd313aac88c763d8ff184011
Tekade Sukant <59193514+tekadesukant@users.noreply.github.com>
2024-07-23 00:08:13 +0530

    Update node_exporter.sh

--- node_exporter.sh
+++ node_exporter.sh
@@ -3,18 +3,36 @@
 # Fetched Latest Node Exporter Version 
 
 
-# Fetch the latest version of Node Exporter from GitHub releases
-latest_version=$(curl -s https://api.github.com/repos/prometheus/node_exporter/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')
-
-# Convert the latest version to uppercase
-latest_version_upper=$(echo $latest_version | tr '[:lower:]' '[:upper:]')
-
 # Construct the download URL using the latest version
-download_url="https://github.com/prometheus/node_exporter/releases/download/${latest_version}/node_exporter-${latest_version}.linux-amd64.tar.gz"
+download_url="https://github.com/prometheus/node_exporter/releases/download/v${latest_version}/node_exporter-${latest_version}.linux-amd64.tar.gz"
 
 # Download and extract Node Exporter
 wget $download_url -O node_exporter.tar.gz
 tar -xf node_exporter.tar.gz
-sudo mv node_exporter-${latest_version}.linux-amd
+sudo mv node_exporter-${latest_version}.linux-amd64/node_exporter /usr/local/bin
+rm -rv node_exporter-${latest_version}.linux-amd64* node_exporter.tar.gz
 
+# Create a system user for Node Exporter
+sudo useradd -rs /bin/false node_exporter
 
+# Create the systemd service file for Node Exporter
+sudo tee /etc/systemd/system/node_exporter.service > /dev/null <<EOF
+[Unit]
+Description=Node Exporter
+After=network.target
+
+[Service]
+User=node_exporter
+Group=node_exporter
+Type=simple
+ExecStart=/usr/local/bin/node_exporter
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Reload systemd, enable and start Node Exporter
+sudo systemctl daemon-reload
+sudo systemctl enable node_exporter
+sudo systemctl start node_exporter.service
+sudo systemctl status node_exporter.service --no-pager
